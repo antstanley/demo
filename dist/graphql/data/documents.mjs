994cb5813ee75ffe6bf93c05c1d42d9087dc9067
Ant Stanley <antstanley@users.noreply.github.com>
2019-02-06 19:29:37 +0000

    set node version

---
+++ dist/graphql/data/documents.mjs
@@ -0,0 +1,55 @@
+export default [
+  {
+    key: 'doc01',
+    name: 'Quick Start',
+    description:
+      'Get up and going with AutoGraphQL with this Quick Start Guide!',
+    status: 'published',
+    internalPath: 'data/docs/QuickStart.md',
+    path: 'quick-start',
+    section: {
+      key: 'sec01',
+      name: 'Documentation',
+      path: '/docs/'
+    }
+  },
+  {
+    key: 'doc02',
+    name: 'Example',
+    description: 'An example Markdown doc to use to double check formatting!',
+    status: 'published',
+    internalPath: 'data/about/docs.md',
+    path: 'pitch-01',
+    section: {
+      key: 'sec02',
+      name: 'About',
+      path: '/about/'
+    }
+  },
+  {
+    key: 'doc03',
+    name: 'Daring Markdown Example',
+    description: 'An example Markdown doc from Daring Fireball',
+    status: 'published',
+    internalPath: 'data/about/daring.md',
+    path: 'pitch-02',
+    section: {
+      key: 'sec02',
+      name: 'About',
+      path: '/about/'
+    }
+  },
+  {
+    key: 'doc04',
+    name: 'Another Daring Markdown Example',
+    description: 'Another example Markdown doc from Daring Fireball',
+    status: 'published',
+    internalPath: 'data/about/daring_again.md',
+    path: 'pitch-03',
+    section: {
+      key: 'sec02',
+      name: 'About',
+      path: '/about/'
+    }
+  }
+]
---
+++ dist/graphql/data/sections.mjs
@@ -0,0 +1,27 @@
+export default [
+  {
+    key: 'sec02',
+    name: 'About',
+    description:
+      "What is AutoGraphQL? Why we're building it. How we're building it. Who we are.",
+    path: '/about/',
+    isNav: true,
+    status: 'published'
+  },
+  {
+    key: 'sec01',
+    name: 'Documentation',
+    description: 'Initial documentation on how this works',
+    path: '/docs/',
+    isNav: true,
+    status: 'published'
+  },
+  {
+    key: 'sec03',
+    name: 'Roadmap',
+    description: 'Projected Feature Roadmap for the product',
+    path: '/road/',
+    isNav: true,
+    status: 'published'
+  }
+]
